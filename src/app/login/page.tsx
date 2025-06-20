8afcd1601c9d8b90c3399f940fef1002fe61714e
Nick Ramsay <NickRamsay1989@gmail.com>
2025-06-20 16:39:57 +1000

    Created initial frontend pages in NextJS

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,17 +2,19 @@
 
 import React, { useState } from 'react';
 
-export default function CreateAccount() {
-  const [email, setDummyHook] = useState("");
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-2xl font-bold">Next.js Mongo Passport Template</h1>
-        <p>Home</p>
-        <input placeholder="Enter email" type="text" onChange={(e) => setDummyHook(e.target.value)}></input>
-        <button>Dummy Button</button>
-        <button onClick={() => window.location.href = "./login"}>Go to Login</button>
+        <input placeholder="Enter email" type="email" onChange={(e) => setEmail(e.target.value)}></input>
+        <input placeholder="Enter password" type="password" onChange={(e) => setPassword(e.target.value)}></input>
+        <button>Login</button>
+        <a href="./create-account-request">Create an Account</a>
+        <a href="./reset-password-request">Reset Password</a>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a target="_blank" href="http://www.github.com/nick-ramsay/nextjs-mongo-passport-template">GitHub</a>
--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -9,10 +9,9 @@ export default function CreateAccount() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-2xl font-bold">Next.js Mongo Passport Template</h1>
-        <p>Home</p>
+        <p>Reset Password</p>
         <input placeholder="Enter email" type="text" onChange={(e) => setDummyHook(e.target.value)}></input>
         <button>Dummy Button</button>
-        <button onClick={() => window.location.href = "./login"}>Go to Login</button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a target="_blank" href="http://www.github.com/nick-ramsay/nextjs-mongo-passport-template">GitHub</a>
