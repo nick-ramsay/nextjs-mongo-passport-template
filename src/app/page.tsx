33e59e104515b585a384d7da56420bed8a9a8c8d
Nick Ramsay <NickRamsay1989@gmail.com>
2025-06-27 10:14:55 +1000

    Fixed responsiveness, centering, and form on login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,8 @@ export default function Home() {
   }, [])
 
 
-  const handleLogin = async (email: string, password: string) => {
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
     try {
       const response = await API.login(email, password)
       window.location.href = '/home';
@@ -29,18 +30,20 @@ export default function Home() {
 
 
   return (
-    <div className="grid items-center justify-center h-screen">
-      <div className="grid-col-1 m-5 p-5 bg-gray-700 max-w-m min-w-sm focus-green rounded text-center">
+    <div className="grid items-center justify-center h-screen w-screen">
+      <div className="grid-col-1 m-5 p-5 bg-gray-700 focus-green rounded text-center xs-sm:w-96 x-sm:w-96 sm:w-96 md:w-full lg:w-full xl:w-full mx-auto">
         <h1 className="text-md font-bold mb-5">Next.js Mongo Passport Template</h1>
-        <div className='mt-3'>
-          <input placeholder="Enter email" type="email" onChange={(e) => setEmail(e.target.value)}></input>
-        </div>
-        <div className='mt-3'>
-          <input placeholder="Enter password" type="password" onChange={(e) => setPassword(sha256(e.target.value))}></input>
-          <div className='mt-5'>
-            <button onClick={() => handleLogin(email, password)}>Login</button>
+        <form onSubmit={handleLogin}>
+          <div className='mt-3'>
+            <input placeholder="Enter email" type="email" onChange={(e) => setEmail(e.target.value)}></input>
           </div>
-        </div>
+          <div className='mt-3'>
+            <input placeholder="Enter password" type="password" onChange={(e) => setPassword(sha256(e.target.value))}></input>
+            <div className='mt-5'>
+              <button type="submit">Login</button>
+            </div>
+          </div>
+        </form>
         <div className='mt-5'>
           <a href="./create-account-request">Create an Account</a>
         </div>
@@ -48,16 +51,14 @@ export default function Home() {
           <a href="./reset-password-request">Reset Password</a>
         </div>
       </div>
-      <div className="grid-col-1 m-5 p-5 max-w-m min-w-sm align-center">
-          <a target="_blank" href="http://www.github.com/nick-ramsay/nextjs-mongo-passport-template">
-            <Image
-            className='justify-self-center'
-              src={GithubLogo}
-              width={80}
-              alt="Picture of the author"
-            />
-          </a>
-       
+      <div className="flex justify-center items-center m-5 p-5 max-w-m min-w-sm">
+        <a target="_blank" href="http://www.github.com/nick-ramsay/nextjs-mongo-passport-template">
+          <Image
+            src={GithubLogo}
+            width={80}
+            alt="GitHub Logo"
+          />
+        </a>
       </div>
     </div>
   );
